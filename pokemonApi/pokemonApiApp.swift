9b05cf395282605f4c17c2716780de6ea8c6a9b7
dgmick <lutmickael@gmail.com>
2022-05-08 22:46:22 +0200

    Initial Commit

---
+++ pokemonApi/pokemonApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokemonApiApp.swift
+//  pokemonApi
+//
+//  Created by Mickael Lutin on 08/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct pokemonApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
